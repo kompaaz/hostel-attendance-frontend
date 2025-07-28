98e5e9d2f58aad1d37bc28b72108d45bd6e4538a
Noel Sebu <noelsebu555@gmail.com>
2025-07-28 20:55:09 +0530

    on visiting root page redirect user to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login")
+  }, []);
+
   return <div>page</div>;
 };
 
 export default page;
-  

66ce88c7b1b8a535856f0904ce2d2ec745516b8f
Joerakesh <rakeshjoe52@gmail.com>
2025-08-01 19:24:15 +0530

    loading screen added

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,21 @@ import { useRouter } from "next/navigation";
 const page = () => {
   const router = useRouter();
   useEffect(() => {
-    router.push("/login")
+    router.push("/login");
   }, []);
 
-  return <div>Sacred Heart Hostel</div>;
+  return (
+    <div>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <div className="flex gap-2">
+          <div className="flex gap-3">
+            <div className="h-7 w-7 border-4 border-t-green-500 border-b-white rounded-full animate-spin"></div>
+          </div>
+          <h1 className="text-2xl">Loading...</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;

ac2f7d233ebab7b93d528913db8c8706486363dc
Noel Sebu <noelsebu555@gmail.com>
2025-09-17 21:15:30 +0530

    added loading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Loading from "./components/Loading";
 
 const page = () => {
   const router = useRouter();
@@ -10,14 +11,15 @@ const page = () => {
 
   return (
     <div>
-      <div className="h-screen w-screen flex items-center justify-center">
+      {/* <div className="h-screen w-screen flex items-center justify-center">
         <div className="flex gap-2">
           <div className="flex gap-3">
             <div className="h-7 w-7 border-4 border-t-green-500 border-b-white rounded-full animate-spin"></div>
           </div>
           <h1 className="text-2xl">Loading...</h1>
         </div>
-      </div>
+      </div> */}
+      <Loading />
     </div>
   );
 };
